64d1300db4485a020daff1f666c2ef463e498551
benjaminkan <50194133+benjaminkan@users.noreply.github.com>
2020-03-05 12:14:10 +0200

    Add files via upload

--- WS4/routes.js
+++ WS4/routes.js
@@ -1,42 +1,67 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var fs = require("fs");
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/demosite/index.html');
+app.use(express.static("public/demosite/"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/demosite/index.html");
 });
 
-app.get('/list', function (req, res) {
-    res.sendFile(__dirname + '/exampledata.txt');
+app.get("/list", function(req, res) {
+  res.sendFile(__dirname + "/exampledata.txt");
 });
 
-app.get('/jsondata', function (req, res) {
-    var data = require('./exampledata2.json');
-    res.json(data);
+app.get("/jsondata", function(req, res) {
+  var data = require("./exampledata2.json");
+  res.json(data);
 });
 
-app.get('/details', function (req, res) {
-    var data = require('./exampledata2.json');
-    
-    var results = '<table border="1">';
+app.get("/details", function(req, res) {
+  var data = require("./exampledata2.json");
 
-    for (var i=0; i < data.length; i++){
-        results +=
-        '<tr>'+
-        '<td>'+data[i].Name+'</td>'+
-        '<td>'+data[i].Email+'</td>'+
-        '</tr>';
-    }
-    res.send(results);
+  var results = '<table border="1">';
+
+  for (var i = 0; i < data.length; i++) {
+    results +=
+      "<tr>" +
+      "<td>" +
+      data[i].Name +
+      "</td>" +
+      "<td>" +
+      data[i].Email +
+      "</td>" +
+      "</tr>";
+  }
+  res.send(results);
 });
 
-app.get('/add', function (req, res) {
-    res.send('Lets try to add some data to a file!');
+app.get("/add", function(req, res) {
+  var data = require("./exampledata2.json");
+
+  data.push({
+    Name: "Benjamin Kanerva",
+    Company: "Laurea",
+    Email: "benjamin@laurea.fi",
+    Date: "30/3/2016 \r\n"
+  });
+
+  var jsonStr = JSON.stringify(data);
+
+  fs.writeFile("exampledata2.json", jsonStr, err => {
+    if (err) throw err;
+    console.log("It's saved!");
+  });
+
+  res.send(
+    "Saved the data to a file. Browse to the /details to see the contents of the file"
+  );
 });
 
-app.get('*', function (req, res) {
-    res.status(404).send('Cant find the requested page');
+app.get("*", function(req, res) {
+  res.status(404).send("Cant find the requested page");
 });
 
 app.listen(8080, function() {
-console.log('Example app listening on port 8080!');
+  console.log("Example app listening on port 8080!");
 });
